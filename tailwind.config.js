76f1c21da1d1032370a5d38805a0fb9986655390
alie <mushyafaali@gmail.com>
2023-12-01 09:37:12 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#ef4444',
+        secondary: '#3b82f6',
+        dark: '#334155'
+      }
+    },
+  },
+  plugins: [],
+}
+
